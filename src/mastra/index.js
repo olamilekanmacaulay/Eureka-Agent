08b41ad8c744ba5a7ac85e0126e4eb91f582a55d
Olalekan Macaulay <olalekan.macaulay1@gmail.com>
2025-11-03 03:57:42 -0500

    a2a route added

--- src/mastra/index.js
+++ src/mastra/index.js
@@ -2,10 +2,25 @@ import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
 import { LibSQLStore } from "@mastra/libsql";
 import { agent as eurekaAgent } from "./agents/householdScienceHelperAgent.js";
+import { a2aAgentRoute } from "./routes/a2a-agent-route.js";
 
 export const mastra = new Mastra({
-  agents: { eureka: eurekaAgent },
+  agents: { 
+    eureka: eurekaAgent 
+  },
   storage: new LibSQLStore({ url: ":memory:" }),
-  logger: new PinoLogger({ name: "Mastra", level: "info" }),
-  observability: { default: { enabled: true } },
+  logger: new PinoLogger({ 
+    name: "Mastra", 
+    level: "debug" 
+  }),
+  observability: { 
+    default: { enabled: true } 
+  },
+  server: {
+    build: {
+      openAPIDocs: true,
+      swaggerUI: true,
+    },
+    apiRoutes: [a2aAgentRoute]
+  }
 });
